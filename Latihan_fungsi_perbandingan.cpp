32eab97c7135540663c8b23f4f11456504655621
Neng Santi <shantyneng05@gmail.com>
2017-03-31 23:17:48 +0700

    Add files via upload

---
+++ Latihan_fungsi_perbandingan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int perbandingan (int a, int b){
+
+if (a>b){
+return a;
+}
+else{
+return b;
+}
+
+}
+
+int main () {
+cout << "hasilnya adalah :" << perbandingan (5,27);
+return 0;
+
+}
+
+
